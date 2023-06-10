4f2dd86d4f2c984e5ae391a8271854f53231552c
Aiya <110978849+219075331@users.noreply.github.com>
2023-06-11 00:23:13 +0900

    UI Basic sample2

--- MainActivity.java
+++ MainActivity.java
@@ -1,40 +1,109 @@
-package com.example.uibasic;
+package com.example.uibasic2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ProgressBar;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
-
-public class MainActivity extends AppCompatActivity
-        implements View.OnClickListener{
-
-    private EditText editTxtName;
-    private TextView txtHello;
-
+public class MainActivity extends AppCompatActivity {
+    private CheckBox checkBoxKimetsu, checkBoxSpy, checkBoxTokyo;
+    private RadioGroup rgMaritalStatus;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnHello = findViewById(R.id.btnHello);
-        btnHello.setOnClickListener(this);
+        checkBoxKimetsu = findViewById(R.id.checkboxKimetsu);
+        checkBoxSpy = findViewById(R.id.checkboxSpy);
+        checkBoxTokyo = findViewById(R.id.checkboxTokyo);
 
-        editTxtName = findViewById(R.id.editTxtName);
-        txtHello = findViewById(R.id.txtHello);
-    }
+        rgMaritalStatus = findViewById(R.id.rgMaritalStatus);
 
-    @Override
-    public void onClick(View view) {
-        if (view.getId()==R.id.btnHello){
-            Toast.makeText(this, "Hello Button Clicked",
-                    Toast.LENGTH_SHORT).show();
-            txtHello.setText("Hello " + editTxtName.getText().toString());
+        progressBar = findViewById(R.id.progressBar);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i = 0; i < 10; i++){
+                    progressBar.incrementProgressBy(10);
+                    SystemClock.sleep(500);
+                }
+            }
+        });
+        thread.start();
+
+        int checkedButton = rgMaritalStatus.getCheckedRadioButtonId();
+        if (checkedButton==R.id.rbMarried){
+            Toast.makeText(MainActivity.this, "Married", Toast.LENGTH_SHORT).show();
+        } else if (checkedButton==R.id.rbSingle) {
+            Toast.makeText(MainActivity.this, "Single", Toast.LENGTH_SHORT).show();
+        } else if (checkedButton==R.id.rbInrel) {
+            Toast.makeText(MainActivity.this, "In a relationship", Toast.LENGTH_SHORT).show();
         }
+
+        rgMaritalStatus.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+                if (checkedId==R.id.rbMarried){
+                    Toast.makeText(MainActivity.this, "Married", Toast.LENGTH_SHORT).show();
+                } else if (checkedId==R.id.rbSingle) {
+                    Toast.makeText(MainActivity.this, "Single", Toast.LENGTH_SHORT).show();
+                } else if (checkedId==R.id.rbInrel) {
+                    Toast.makeText(MainActivity.this, "In a relationship", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        checkBoxKimetsu.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (isChecked){
+                    Toast.makeText(MainActivity.this,
+                            "You have watched Kimetsu no Yaiba", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(MainActivity.this,
+                            "You NEED to watch Kimetsu no Yaiba",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        checkBoxSpy.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked){
+                    Toast.makeText(MainActivity.this,
+                            "You have watched Spy Family", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(MainActivity.this,
+                            "You NEED to watch Spy Family", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        checkBoxTokyo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked){
+                    Toast.makeText(MainActivity.this,
+                            "You have watched Tokyo Revengers", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(MainActivity.this,
+                            "You NEED to watch Tokyo Revengers", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+
     }
 }
